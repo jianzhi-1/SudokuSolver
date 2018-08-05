131c75ba8b1d046b23fe0dbe0275d216a3793bbe
jianzhi <scientistclub1000@gmail.com>
2018-08-05 20:01:01 +0800

    started repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# SudokuSolver
+
+Attempting to complete this project before Prelims (before 09/09/18)
---
+++ sudokusolver.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int main(){
+	
+}
